25c4d3e54b2d7ac7fcae2c609525fdecd7544589
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2016-05-15 21:55:08 -0700

    Test Cube and Empty Script
    Push test

---
+++ Assets/RandomTestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Testing a code push to github
+
+public class RandomTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
