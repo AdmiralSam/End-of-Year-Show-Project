065adb2ae8a2d78445d59e7e8ec08bc0f28de3de
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2016-05-20 22:46:53 -0700

    Added GhostShooter Scene

---
+++ Assets/WeakSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeakSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
